ffbb0330fd308031354e32a428af200c58de88d7
pritam <devpritx@gmail.com>
2024-12-25 01:58:31 +0530

    Initial Variables Declare

---
+++ Number Gussing Game/main.py
@@ -0,0 +1,23 @@
+# Number Guessing Game
+
+
+def main():
+    # Variables
+    random_number = None
+    player_input = None
+    Hint = None
+    attempts = None
+    levels = {
+        "easy": {
+            "attempts": "unlimited"
+        },
+        "medium": {
+            "attempts": 10
+        },
+        "hard": {
+            "attempts": 5
+        }
+    }
+    
+if __name__ == "__main__":
+    main()
